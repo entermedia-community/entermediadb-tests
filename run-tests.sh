471eb3b6096cf0fc98225284732dae3609333977
Thomas Nadalie <thomas.nadalie@epitech.eu>
2018-09-19 14:18:25 -0500

    Overwrite results file

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,4 @@
 
 [[ -d dir ]] || mkdir results
 
-./apache-jmeter-4.0/bin/jmeter -n -t MediaDB/upload_script.jmx -l results/resulttest.csv
+./apache-jmeter-4.0/bin/jmeter -f -n -t MediaDB/upload_script.jmx -l results/resulttest.csv
