66badbeac5e3089cc4d79894abc0ce0b6696ac9e
Matti <Matti@Surface>
2018-10-10 18:26:45 -0500

    script updates

--- run-em9devtests.sh
+++ run-em9devtests.sh
@@ -2,4 +2,4 @@
 
 [[ -d dir ]] || mkdir results
 
-./apache-jmeter-4.0/bin/jmeter -f -n -t MediaDB/upload_script.jmx -l results/resulttest.csv
+./apache-jmeter-4.0/bin/jmeter -f -n -t MediaDB/upload_script_em9dev.jmx -l results/resulttest.csv
