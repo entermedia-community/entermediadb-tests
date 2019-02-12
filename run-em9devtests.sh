7375697b603334b3676f82ee66347e73e6c21e4c
Thomas N <t.nadalie33@gmail.com>
2019-02-12 14:41:13 -0600

    Fix missing result file

--- run-em9devtests.sh
+++ run-em9devtests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 [[ -d dir ]] || mkdir results
+[[ -f dir/resulttest.csv ]] || touch results/resulttest.csv
 
 ./apache-jmeter-4.0/bin/jmeter -f -n -t MediaDB/upload_script_em9dev.jmx -l results/resulttest.csv
