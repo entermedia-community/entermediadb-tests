a8d77a514575a56b1de51c7285d7a11dc1bc2959
Thomas N <t.nadalie33@gmail.com>
2019-04-12 11:01:34 -0500

    Fix wrong branch

--- merge-release.sh
+++ merge-release.sh
@@ -6,42 +6,47 @@ EM_CORE="$WORKSPACE/entermedia-core"
 EXT_OPENDIT="$WORKSPACE/extension-openedit"
 DEMOALL="$WORKSPACE/demoall"
 
+cd $WORKSPACE
 if [ ! -d "$APP_EMSHARE" ]; then
   git clone git@github.com:entermedia-community/app-emshare.git
 fi
 cd $APP_EMSHARE
-git checkout -b origin/entermedia9
+git checkout -b entermedia9
 git merge origin/em9dev
-git push --set-upstream origin origin/entermedia9
+git push origin entermedia9
 
+cd $WORKSPACE
 if [ ! -d "$EM_SERVER" ]; then
   git clone git@github.com:entermedia-community/entermedia-server.git
 fi
 cd $EM_SERVER
-git checkout -b origin/entermedia9
+git checkout -b entermedia9
 git merge origin/em9dev
-git push --set-upstream origin origin/entermedia9
+git push origin entermedia9
 
+cd $WORKSPACE
 if [ ! -d "$EM_CORE" ]; then
   git clone git@github.com:entermedia-community/entermedia-core.git
 fi
 cd $EM_CORE
-git checkout -b origin/entermedia9
+git checkout -b entermedia9
 git merge origin/em9dev
-git push --set-upstream origin origin/entermedia9
+git push origin entermedia9
 
+cd $WORKSPACE
 if [ ! -d "$EXT_OPENDIT" ]; then
   git clone git@github.com:entermedia-community/extension-openedit.git
 fi
 cd $EXT_OPENDIT
-git checkout -b origin/entermedia9
+git checkout -b entermedia9
 git merge origin/em9dev
-git push --set-upstream origin origin/entermedia9
+git push origin entermedia9
 
+cd $WORKSPACE
 if [ ! -d "$DEMOALL" ]; then
   git clone git@github.com:entermedia-community/demoall.git
 fi
 cd $DEMOALL
-git checkout -b origin/entermedia9
+git checkout -b entermedia9
 git merge origin/em9dev
-git push --set-upstream origin origin/entermedia9
+git push origin entermedia9
