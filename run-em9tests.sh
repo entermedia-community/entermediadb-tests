125bc0d09c09aeea56ac1c4de91fe46dc671e438
Thomas N <t.nadalie33@gmail.com>
2019-04-17 09:45:52 -0500

    Parse result csv to detect error

--- run-em9tests.sh
+++ run-em9tests.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 [[ -d dir ]] || mkdir results
+[[ -f dir/resulttest.csv ]] || touch results/resulttest.csv
 
 ./apache-jmeter-4.0/bin/jmeter -f -n -t MediaDB/upload_script.jmx -l results/resulttest.csv
+
+while IFS=',' read -r a1 a2 a3 httpCode a5
+do
+  if [ "$httpCode" == "500" ]; then
+    exit 1
+  fi
+
+done < results/resulttest.csv
+exit 0
