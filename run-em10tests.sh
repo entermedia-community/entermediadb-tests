e0a166ca95801f5803b226d9f984b75a061b96d2
Cristobal Mejia <cristobal@openedit.org>
2020-10-29 21:05:00 -0600

    script to run

---
+++ run-em10tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+[[ -d dir ]] || mkdir results
+[[ -f dir/results10.csv ]] || touch results/results10.csv
+
+./apache-jmeter-5.3/bin/jmeter -f -n -t em2/emshare2-em10.jmx -l results/results10.csv
+
+while IFS=',' read -r a1 a2 a3 httpCode a5
+do
+  if [ "$httpCode" == "500" ]; then
+    exit 1
+  fi
+
+done < results/results10.csv
+exit 0
