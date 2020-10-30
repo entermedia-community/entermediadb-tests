acb81bd72c115734588675b44b6db75566580598
Cristobal Mejia <cristobal@openedit.org>
2020-10-30 14:50:44 -0600

    Passing Var

--- run-em10tests.sh
+++ run-em10tests.sh
@@ -5,7 +5,7 @@ echo "$BUILD_NUMBER"
 [[ -d dir ]] || mkdir results
 [[ -f dir/results10.csv ]] || touch results/results10.csv
 
-./apache-jmeter-5.3/bin/jmeter -f -n -t em2/emshare2-em10.jmx -l results/results10.csv
+./apache-jmeter-5.3/bin/jmeter -f -n -t em2/emshare2-em10.jmx -l results/results10.csv -JbuildNumber=$BUILD_NUMBER
 
 while IFS=',' read -r a1 a2 a3 httpCode a5
 do
