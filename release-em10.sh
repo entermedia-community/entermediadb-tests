169082ac9565dc9d1e196c59792e814d5c21a4af
Cristobal Mejia <cristobal@openedit.org>
2020-11-03 16:12:40 -0600

    ln backwards?

--- release-em10.sh
+++ release-em10.sh
@@ -5,4 +5,4 @@
 JOB_NAME="em10_demoall"
 LASTSUCCESS_DEMOALL=$(cat ../../../jobs/$JOB_NAME/builds/permalinks | grep lastSuccessfulBuild | sed 's/lastSuccessfulBuild //')
 WAR_FILE="../../../jobs/$JOB_NAME/builds/$LASTSUCCESS_DEMOALL/archive/deploy/ROOT.war"
-ln -s  "../builds/$BUILD_NUMBER/archive/ $WAR_FILE"
+ln -s  "$WAR_FILE ../builds/$BUILD_NUMBER/archive/ROOT.war"
