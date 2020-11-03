1e8f25df658ab3e4fc91e7dceae73555c4ba0be7
Cristobal Mejia <cristobal@openedit.org>
2020-11-02 18:22:45 -0600

    exit code

--- run-em10tests.sh
+++ run-em10tests.sh
@@ -6,6 +6,7 @@ echo "$BUILD_NUMBER"
 [[ -f dir/results10.csv ]] || touch results/results10.csv
 
 ./apache-jmeter-5.3/bin/jmeter -f -n -t em2/emshare2-em10.jmx -l results/results10.csv -JbuildNumber=$BUILD_NUMBER
+JMEXIT=$?
 
 while IFS=',' read -r a1 a2 a3 httpCode a5
 do
@@ -14,4 +15,4 @@ do
   fi
 
 done < results/results10.csv
-exit 0
+exit $?
