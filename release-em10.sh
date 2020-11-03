d8721ffb96882539e9d443c96e60ee11e405f7d8
Cristobal Mejia <cristobal@openedit.org>
2020-11-03 14:11:11 -0600

    release script

---
+++ release-em10.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+#-$BUILD_NUMBER Current project Build Number from Jenkins
+
+JOB_NAME="em10_demoall"
+LASTSUCCESS_DEMOALL=$(cat ../../../jobs/$JOB_NAME/builds/permalinks | grep lastSuccessfulBuild | sed 's/lastSuccessfulBuild //')
+WAR_FILE="../../../jobs/$JOB_NAME/builds/$LASTSUCCESS_DEMOALL/archive/deploy/ROOT.war"
+ln -s  "$WAR_FILE ../builds/$BUILD_NUMBER/archive/"
