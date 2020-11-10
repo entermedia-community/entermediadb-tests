7cb1f22879d357c67bcb86a27089be19d16af8cb
Cristobal Mejia <cristobal@openedit.org>
2020-11-10 17:23:29 -0600

    New path

--- release-em10.sh
+++ release-em10.sh
@@ -5,4 +5,5 @@
 JOB_NAME="em10_demoall"
 LASTSUCCESS_DEMOALL=$(cat ../../../jobs/$JOB_NAME/builds/permalinks | grep lastSuccessfulBuild | sed 's/lastSuccessfulBuild //')
 WAR_FILE="../../../jobs/$JOB_NAME/builds/$LASTSUCCESS_DEMOALL/archive/deploy/ROOT.war"
-cp -rp  $WAR_FILE .
+mkdir deploy
+cp -rp  $WAR_FILE deploy/.
